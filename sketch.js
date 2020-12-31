67445a4239b7f157d6b9ac0e22bd354ab8f0ee18
adhvikgamngZ <72864018+adhvikgamngZ@users.noreply.github.com>
2020-12-31 15:36:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var bullet,wall;
+var speed,weight,thickness;
+function setup() {
+  createCanvas(1300,500);
+  speed=random(55,90);
+  weight=random(400,1500);
+  thickness=random(22,83);
+  bullet=createSprite(50, 200, 50, 50);
+  bullet.velocityX=speed;
+  wall=createSprite(1200, 200, thickness, height/2);
+  wall.shapeColor=color(80,80,80);
+}
+function draw() {
+  background(0,0,0);  
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2) {
+    bullet.velocityX=0;
+    var deformation=0.5 * weight * speed * speed/22509;
+    if(deformation>180)
+    {
+      bullet.shapeColor=color(255,0,0);
+    }
+
+    if(deformation<180 && deformation>80) {
+      bullet.shapeColor=color(230,230,0)
+    }
+    if(deformation,80) {
+      bullet.shapeColor=color(0,255,0);
+    }
+  }
+  drawSprites();
+}
